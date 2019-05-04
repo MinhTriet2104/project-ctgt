e34bd07a83751c47815d18a94aa342189b147c25
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-05-04 10:50:33 +0700

    Check và chỉnh sửa code của Văn

--- Source.cpp
+++ Source.cpp
@@ -36,6 +36,34 @@ struct Node
 	User xData;
 	Node *pNext;
 };
+struct AdminNode
+{
+	Administrators xData;
+	AdminNode *pNext;
+};
+struct AdminList
+{
+	AdminNode *pHead;
+	AdminNode *pTail;
+	AdminList()
+	{
+		pHead = pTail = NULL;
+	}
+};
+struct EmployeeNode
+{
+	Employee xData;
+	EmployeeNode *pNext;
+};
+struct EmployeeList
+{
+	EmployeeNode *pHead;
+	EmployeeNode *pTail;
+	EmployeeList()
+	{
+		pHead = pTail = NULL;
+	}
+};
 struct List
 {
 	Node *pHead;
@@ -45,29 +73,51 @@ struct List
 		pHead = pTail = NULL;
 	}
 };
-
 //khai bao nguyen mau ham con
 Node *CreateNode(User xUS);
+AdminNode *CreateNodeAdmin(Administrators xAdmin);
+EmployeeNode *CreateNodeEmployee(Employee xEmployee);
 void addTail(List &L, User xUS);
+void addTailAdmin(AdminList &L, Administrators xAdmin);
+void addTailEmployee(EmployeeList &L, Employee xEmployee);
 void login(User xUS);
-void inputList(List &L, User &xUS);
-void output1user(List L);
-void output(List L);
-void docFile(List &L ,int nN);
+void outputUser(List L);
+void outputUsers(List L);
+void docFileUsers(List &L);
+void docFileAdmins(AdminList &L);
+void docFileEmployees(EmployeeList &L);
 //ham main
 void main()
 {
-	List L;
-	User xUS;
-	login(xUS);
-	/*input(L, xUS);*/
+	List userL;
+	AdminList AdminL;
+	EmployeeList EmployeeL;
 	
-	inputList(L, xUS);
+	docFileAdmins(AdminL);
+	docFileEmployees(EmployeeL);
+	docFileUsers(userL);
 	
-	output(L);
+	outputUsers(userL);
+
 	system("pause");
 }
 //dinh nghia ham con
+AdminNode *CreateNodeAdmin(Administrators xAdmin) {
+	AdminNode *p = new AdminNode;
+	if (p == NULL)
+		exit(1);
+	p->xData = xAdmin;
+	p->pNext = NULL;
+	return p;
+}
+EmployeeNode *CreateNodeEmployee(Employee xEmployee) {
+	EmployeeNode *p = new EmployeeNode;
+	if (p == NULL)
+		exit(1);
+	p->xData = xEmployee;
+	p->pNext = NULL;
+	return p;
+}
 Node *CreateNode(User xUS)
 {
 	Node *p = new Node;
@@ -86,27 +136,13 @@ void login(User xUS)
 	rewind(stdin);
 	getline(cin, xUS.sPassword);
 }
-void docFile(List &L ,int nN)
+void docFileUsers(List &L)
 {
-
 	ifstream fcin;
-	fcin.open("Text.DAT", ios::in);
-	if(fcin.is_open() == true)
+	fcin.open("user.DAT");
+	while (fcin.eof() != 1)
 	{
-		cout<<" Doc file thanh cong";
-
-	}
-	else
-	{
-		cout<<" Doc file khong thanh cong ";
-	}
-	User xUS;
-	Node *p = L.pHead;
-	p=CreateNode(xUS);
-	for(int i =0;i<nN;i++)
-	{
-		
-		
+		User xUS;
 		rewind(stdin);
 		getline(fcin, xUS.sAccount, '#');
 		rewind(stdin);
@@ -125,6 +161,34 @@ void docFile(List &L ,int nN)
 	}
 	fcin.close();
 }
+void docFileAdmins(AdminList &L) {
+	ifstream fcin;
+	fcin.open("administrators.dat");
+	while (fcin.eof() != 1)
+	{
+		Administrators xAdmin;
+		rewind(stdin);
+		getline(fcin, xAdmin.sAcount_Admin, '#');
+		rewind(stdin);
+		getline(fcin, xAdmin.sPassword_Admin);
+		addTailAdmin(L, xAdmin);
+	}
+	fcin.close();
+}
+void docFileEmployees(EmployeeList &L) {
+	ifstream fcin;
+	fcin.open("employee.dat");
+	while (fcin.eof() != 1)
+	{
+		Employee xEmployee;
+		rewind(stdin);
+		getline(fcin, xEmployee.sAccount_user, '#');
+		rewind(stdin);
+		getline(fcin, xEmployee.sPassword_user);
+		addTailEmployee(L, xEmployee);
+	}
+	fcin.close();
+}
 void addTail(List &L, User xUS)
 {
 	Node *pNew = CreateNode(xUS);
@@ -138,36 +202,47 @@ void addTail(List &L, User xUS)
 		L.pTail = pNew;
 	}
 }
-void output1user(User xUS)
+void addTailAdmin(AdminList &L, Administrators xAdmin) {
+	AdminNode *pNew = CreateNodeAdmin(xAdmin);
+	if (L.pHead == NULL)
+	{
+		L.pHead = L.pTail = pNew;
+	}
+	else
+	{
+		L.pTail->pNext = pNew;
+		L.pTail = pNew;
+	}
+}
+void addTailEmployee(EmployeeList &L, Employee xEmployee) {
+	EmployeeNode *pNew = CreateNodeEmployee(xEmployee);
+	if (L.pHead == NULL)
+	{
+		L.pHead = L.pTail = pNew;
+	}
+	else
+	{
+		L.pTail->pNext = pNew;
+		L.pTail = pNew;
+	}
+}
+void outputUser(User xUS)
 {
 	cout<<"\n Account: "<< xUS.sAccount<<endl;
-		cout<<" Password: "<< xUS.sPassword <<endl;
-		cout<<"\n Ho Ten: "<<  xUS.sName <<endl;
-		cout<<"\n Tuoi: "<<  xUS.nAge <<endl;
-		cout<<"\n Address: "<< xUS.sAddress <<endl;
-		cout<<"\n nNumberPhone: "<<  xUS.nNumberPhone <<endl;
-		cout<<"\n MailAddress: "<<  xUS.sMailAddress<<endl;
+	cout<<" Password: "<< xUS.sPassword <<endl;
+	cout<<"\n Ho Ten: "<<  xUS.sName <<endl;
+	cout<<"\n Tuoi: "<<  xUS.nAge <<endl;
+	cout<<"\n Address: "<< xUS.sAddress <<endl;
+	cout<<"\n nNumberPhone: "<<  xUS.nNumberPhone <<endl;
+	cout<<"\n MailAddress: "<<  xUS.sMailAddress<<endl;
 }
-void output(List L)
+void outputUsers(List L)
 {
 	User xUS;
 	cout << "\n______________XUAT DS______________\n";
 	for(Node *p=L.pHead;p!=NULL;p=p->pNext)
 	{
-		output1user(p->xData);
+		outputUser(p->xData);
 		cout<<endl;
-	}
-
-}
-void inputList(List &L, User &xUS)
-{
-	Node *p = L.pHead;
-	int nN = 4;
-	docFile(L, nN);
-	for (p;p!=NULL;p=p->pNext)
-	{
-		
-		p=CreateNode(xUS);
-		addTail(L, xUS);
 	}
 }
